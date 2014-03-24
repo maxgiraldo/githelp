9f5469872f5d837e886f433c392c76a7a9ee7a74
Ji Ho Koo <jiho.koo@outlook.com>
2014-03-24 16:07:06 -0400

    extension enable git queries

--- app/chrome_extension/scripts/chromeControllers/popup.js
+++ app/chrome_extension/scripts/chromeControllers/popup.js
@@ -33,5 +33,23 @@ chrome.controller('PopupController', function ($scope) {
       }
     };
 
+    $scope.searchUser = function(){
+      console.log("hello")
+      var params = "queryInput="+$scope.queryInput;
+      console.log(params);
+      http.open("POST", "http://192.168.1.178:3000/query", true);
+      // make request like this to appointment with all the
+      // relevant details
+      // then when the server gets the message, save
+      http.setRequestHeader('Access-Control-Allow-Origin', '*');
+      http.setRequestHeader('Access-Control-Allow-Methods', 'POST');
+      http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+      // http.setRequestHeader("Content-length", params.length);
+      // http.setRequestHeader("Connection", "close");
+
+      http.send(params);
+    }
+
   }
 );
