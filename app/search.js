4eea549613b67ce48dbc5148a33284c37e5e1f45
Waine Tam <wainetam@gmail.com>
2014-03-25 12:13:08 -0400

    added search func and scraper

--- app/search.js
+++ app/search.js
@@ -1,40 +1,50 @@
 var GitHubApi = require('github'),
-    _ = require('underscore'),
-    async = require('async'),
-    Q = require('q'),
-    request = require('request');
+  _ = require('underscore'),
+  async = require('async'),
+  Q = require('q'),
+  request = require('request');
 
 var github = new GitHubApi({
   // required
-    version: '3.0.0',
-    // optional
-    // debug: true,
-    // protocol: 'https',
-    // host: 'github.my-GHE-enabled-company.com',
-    // pathPrefix: '/api/v3', // for some GHEs
-    // timeout: 5000
+  version: '3.0.0',
+  // optional
+  // debug: true,
+  // protocol: 'https',
+  // host: 'github.my-GHE-enabled-company.com',
+  // pathPrefix: '/api/v3', // for some GHEs
+  // timeout: 5000
 });
 
 // ** Auth (basic)
 
 var authenticate = function() {
-    github.authenticate({
-        type: 'oauth',
-        token: 'b7eeb22a78df8209f95c24c3448c6fcbce9e636c' // personal token
-    });
+  github.authenticate({
+    type: 'oauth',
+    token: 'b7eeb22a78df8209f95c24c3448c6fcbce9e636c' // personal token
+  });
 };
+
+var oAuth = {
+  type: 'oauth',
+  token: 'b7eeb22a78df8209f95c24c3448c6fcbce9e636c' // personal token
+}
+var basicAuth = {
+  type: 'basic',
+  username: 'wainetam',
+  password: 'wainetam14'
+}
 
 authenticate();
 
 // ** User information
 
 var User = function(username) {
-    this.user = username;
-    this.per_page = 50;
-    this.q = username;
+  this.user = username;
+  this.per_page = 50;
+  this.q = username;
 };
 
-// var user = new User('visionmedia');
+
 
 // github.user.getFollowers(user, function(err, followers) { // need to get followers
 //     if(err) { console.log(err); }
@@ -42,11 +52,36 @@ var User = function(username) {
 //     console.log('USER follower data ', followers);
 // });
 
-// github.repos.getFromUser(user, function(err, repoData) { // get # of repos
-//     if(err) { console.log(err); }
+var getUserRepos = function(username) {
+  var deferred = Q.defer();
+  var user = new User(username);
 
-//     console.log('USER REPO data ', repoData);
-// });
+  github.repos.getFromUser(user, function(err, repoData) { // get # of repos
+    if(err) {
+      console.log(err);
+      deferred.reject(err);
+    }
+
+    userRepos = [];
+    repoData.forEach(function(repo) {
+      var repoObj = {
+        name: repo.name,
+        stars: repo.stargazers_count // stars
+      };
+      userRepos.push(repoObj);
+    });
+    console.log('USER REPOS ', userRepos);
+
+    var sortedUserRepos = _.sortBy(userRepos, function(repo) { return repo.watchers; });
+    console.log('SORTED USER REPOS ', sortedUserRepos.reverse());
+
+    deferred.resolve(sortedUserRepos.reverse());
+    // console.log('USER REPO DHH data ', repoData);
+  });
+  return deferred.promise;
+};
+
+// getUserRepos('dhh');
 
 // github.events.getFromUser(user, function(err, eventData) { // get activity
 //     if(err) { console.log(err); }
@@ -62,35 +97,36 @@ var User = function(username) {
 
 
 exports.userStats = function(username) {
-    var deferred = Q.defer();
-    console.log('IN USER STATS');
-    var searchObj = { url: 'https://api.github.com/users/' + username, headers: { 'User-Agent': 'wainetam' }, auth: { 'type': 'basic', 'username': 'wainetam', 'password': 'wainetam14'} };
-    request(searchObj, function(err, response, userData) {
-        console.log('IN USER STATS post REQUEST ', userData.email);
-        if(err && response.statusCode !== 200) {
-            console.log('Request error.');
-            deferred.reject(err);
-        }
-        userData = JSON.parse(userData);
-        console.log('SOLE USER ', userData);
-        console.log('USER EMAIL ', userData.email);
-        console.log('USER BLOG ', userData.blog);
-        console.log('USER FOLLOWER COUNT ', userData.followers);
-        console.log('USER REPO COUNT ', userData.public_repos);
-        console.log('USER GIST COUNT ', userData.public_gists);
-        var userObj = {
-            email: userData.email || '',
-            blog: userData.blog,
-            followers: userData.followers,
-            repos: userData.public_repos,
-            gists: userData.public_gists
-        };
-        deferred.resolve(userObj);
-    });
-    return deferred.promise;
+  var deferred = Q.defer();
+  console.log('IN USER STATS');
+  var searchObj = { url: 'https://api.github.com/users/' + username, headers: { 'User-Agent': 'wainetam' }, auth: basicAuth };
+  request(searchObj, function(err, response, userData) {
+    console.log('IN USER STATS post REQUEST ', userData.email);
+    if(err && response.statusCode !== 200) {
+      console.log('Request error.');
+      deferred.reject(err);
+    }
+    userData = JSON.parse(userData);
+    console.log('SOLE USER ', userData);
+    console.log('USER EMAIL ', userData.email);
+    console.log('USER BLOG ', userData.blog);
+    console.log('USER FOLLOWER COUNT ', userData.followers);
+    console.log('USER REPO COUNT ', userData.public_repos);
+    console.log('USER GIST COUNT ', userData.public_gists);
+
+    var userObj = {
+      email: userData.email || '',
+      blog: userData.blog,
+      followers: userData.followers,
+      repos: userData.public_repos,
+      gists: userData.public_gists
+    };
+    deferred.resolve(userObj);
+  });
+  return deferred.promise;
 };
 
-// userStats('visionmedia');
+exports.userStats('dhh');
 
 // github.user.get(user, function(err, data) {
 //     if(err) { console.log(err); }
@@ -101,103 +137,178 @@ exports.userStats = function(username) {
 // });
 
 
+
+
 // ** Contributors filter
 
 var topContributors = function(contributorsArr, threshold) { //threshold in percent of total contributions to repo (i.e., 5)
-    var totalContributions = 0; // of top 100 (param determined in repo query)
-    var topContributors = [];
-    contributorsArr.forEach(function(user) {
-        totalContributions = totalContributions + user.contributions;
-    });
-    console.log('total contributions of top 100 ', totalContributions);
+  var totalContributions = 0; // of top 100 (param determined in repo query)
+  var topContributors = [];
+  contributorsArr.forEach(function(user) {
+    totalContributions = totalContributions + user.contributions;
+  });
+  console.log('total contributions of top 100 ', totalContributions);
 
-    contributorsArr.forEach(function(user) {
-        if(user.contributions/totalContributions > threshold/100 )
-        topContributors.push(user);
-    });
-    console.log('len of topContribs', topContributors.length);
-    return topContributors;
+  contributorsArr.forEach(function(user) {
+    if(user.contributions/totalContributions > threshold/100 )
+    topContributors.push(user);
+  });
+  console.log('len of topContribs', topContributors.length);
+  return topContributors;
 };
 
 var findOtherTop = function(contributorsArr, thresholdObj) { // threshold: {followers: 500, repos: 100, gists: 100}
-
-    async.map(contributorsArr, function(user, callback) {
-        console.log('IN MAP USER ', user);
-        console.log('IN MAP USER LOGIN ', user.login);
-        exports.userStats(user.login).then(function(userObj) {
-            console.log('IN MAP ', userObj);
-            callback(null, userObj);
-        });
-    }, function(err, userObjArr) {
-        console.log('IN FINDOTHERTOP ', userObjArr);
-        var otherTop = [];
-        userObjArr.forEach(function(user) {
-            if(user.followers > thresholdObj.followers || user.repos > thresholdObj.repos || user.gists > thresholdObj.gists ) {
-                otherTop.push(user);
-            }
-        });
-        console.log('OTHERTOP RESULTS', otherTop);
+  var deferred = Q.defer();
+  async.map(contributorsArr, function(user, callback) {
+    console.log('IN MAP USER ', user);
+    console.log('IN MAP USER LOGIN ', user.login);
+    exports.userStats(user.login).then(function(userObj) {
+      console.log('IN MAP ', userObj);
+      callback(null, userObj);
     });
+  }, function(err, userObjArr) {
+    if(err) { deferred.reject(err); }
+    console.log('IN FINDOTHERTOP ', userObjArr);
+    var otherTop = [];
+    userObjArr.forEach(function(user) {
+      if(user.followers > thresholdObj.followers || user.repos > thresholdObj.repos || user.gists > thresholdObj.gists ) {
+        otherTop.push(user);
+      }
+    });
+    console.log('OTHERTOP RESULTS', otherTop);
+    deferred.resolve(otherTop);
+  });
+  return deferred.promise;
 };
 
 // ** Repo info
 
+var processRepoUrl = function(repoUrl) { // wainetam/myRepo
+  var user = repoUrl.match(/github.com\/(.+)\/.+/)[1];
+  var repo = repoUrl.match(/github.com\/.+\/(.+)/)[1];
+  return {
+    user: user,
+    repo: repo
+  };
+};
+
 var Repo = function(author, repo) {
-    this.user = author; // author
-    this.repo = repo;
-    this.page = 1; // optional; page number of results to fetch
-    this.per_page = 100; // optional; 30 is default
+  this.user = author; // author
+  this.repo = repo;
+  this.page = 1; // optional; page number of results to fetch
+  this.per_page = 100; // optional; 30 is default
 };
 //
 // var repo = new Repo('twbs', 'bootstrap'); // user, repo name
 
-exports.getContributors = function(author, repo) {
-    var repo = new Repo(author, repo);
 
-    github.repos.getContributors(repo, function(err, users) {
-        if(err) { console.log(err); }
-        // console.log('twbs contributors ', data[50]);
+exports.repoStats = function(repoUrl) {  // rails/rails
+  var deferred = Q.defer();
 
-        var coreTeam = topContributors(users, 1);
-        console.log('CORE ', coreTeam); // of top 100 users, returns those w/ at least 1% of the total contribs of top 100
+  var obj = processRepoUrl(repoUrl);
 
-        var otherTop = findOtherTop(users, {followers: 500, repos: 100, gists: 100});
-        console.log('OTHERTOP ', otherTop);
+  github.repos.get(obj, function(err, data) {
+    if(err) {
+      console.log(err);
+      deferred.reject(err);
+    }
+
+    var repoObj = {
+      watchers: data.subscribers_count, // watchers
+      stars: data.stargazers_count, // stars
+      url: data.html_url,
+      description: data.description,
+      owner: data.owner.login,
+      forks: data.forks
+    };
+
+    console.log('SOLE REPO OBJ ', repoObj);
+
+    deferred.resolve(repoObj);
+  });
+  return deferred.promise;
+};
+
+// repoStats('https://www.github.com/rails/rails');
+
+
+var getContributors = function(author, repo) {
+  var deferred = Q.defer();
+  var repoQ = new Repo(author, repo);
+
+  github.repos.getContributors(repoQ, function(err, users) {
+    if(err) { console.log(err); }
+    // console.log('twbs contributors ', data[50]);
+    console.log('USERS ', users);
+    var coreTeam = topContributors(users, 1);
+    console.log('CORE ', coreTeam); // of top 100 users, returns those w/ at least 1% of the total contribs of top 100
+
+    var otherTop = findOtherTop(users, {followers: 500, repos: 100, gists: 100});
+    findOtherTop(users, {followers: 500, repos: 100, gists: 100}).then(function(otherTop) {
+      console.log('OTHERTOP ', otherTop);
+      var contributorsObj = {
+        coreTeam: coreTeam,
+        otherTop: otherTop
+      };
+      console.log('CONTRIBUTORS OBJ', contributorsObj);
+      deferred.resolve(contributorsObj);
     });
+  });
+  return deferred.promise;
 };
 
 var Query = function(query) {
-    this.q = query;
-    // ,
-    // 'sort': , optional
-    // 'order': optional
+  this.q = query;
+  // ,
+  // 'sort': , optional
+  // 'order': optional
 };
 
-exports.query = function(queryString) {
-    var deferred = Q.defer();
-    var query = new Query(queryString); // search string
+exports.query = function(queryString, repoBoolean, userBoolean) { // repo and userBool are optional params and can't be both true
+  var deferred = Q.defer();
+  var query = new Query(queryString); // search string
 
-    github.search.repos(query, function(err, data) { // repos
-        if(err) {
-            console.log(err);
-            deferred.reject(err);
-        }
-        console.log('query output ', data);
-        deferred.resolve(data);
-    });
-    return deferred.promise;
+  // returns general query
+  github.search.repos(query, function(err, data) { // repos
+    if(err) {
+      console.log(err);
+      deferred.reject(err);
+    }
+    console.log('query output ', data);
+
+    var urlObj = processRepoUrl('http://github.com/rails/rails'); // dummy variable for now
+    console.log('URLOBJ', urlObj);
+
+    // if specific repo query
+
+    if(repoBoolean) {
+      getContributors(urlObj.user, urlObj.repo).then(function(contributorsObj) {
+        deferred.resolve(contributorsObj);
+      });
+    }
+    // if specific user query
+
+    else if(userBoolean) {
+      exports.userStats(urlObj.user).then(function(userObj) {
+        deferred.resolve(userObj);
+      });
+    }
+
+    else {
+      deferred.resolve(data);
+    }
+  });
+  return deferred.promise;
 };
 
-exports.extractUserAndRepo = function(partialUrl) { // wainetam/myRepo
-    var user = partialUrl.match(/(.+)\/.+/)[0];
-    var repo = partialUrl.match(/.+\/(.+)/)[0];
-    return {
-        user: user,
-        repo: repo
-    };
-};
 
 // github.repos.getContributors(repo, function(err, data) {
 //     if(err) { console.log(err); }
 //     console.log('twbs contributors ', data);
 // });
+
+
+// top contribs & other top
+// avail times & prices
+// repos in order of pop
+// other repos they are contribs
