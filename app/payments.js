6efd030d2d8698ff2f0dfeeb416770b22960e693
Waine Tam <wainetam@gmail.com>
2014-03-25 16:19:41 -0400

    added payments

--- app/payments.js
+++ app/payments.js
@@ -1,20 +1,26 @@
-var balanced = require('balanced-official');
+// var balanced = require('balanced-official');
 
-balanced.configure('ak-test-1dsNimzLa65kRDXzRzGgLQ5Gqoi8sIwCU');
+// balanced.configure('ak-test-1dsNimzLa65kRDXzRzGgLQ5Gqoi8sIwCU');
 
-var customer = balanced.marketplace.customers.create();
+// var customer = balanced.marketplace.customers.create();
 
-var card = balanced.marketplace.cards.create({
-    'number': '4111111111111111',
-    'expiration_year': '2016',
-    'expiration_month': '12',
-     'cvv': '123'
-});
+// var card = balanced.marketplace.cards.create({
+//   'uri': '/cards',
+//   'payload': {
+//     'expiration_month': '12',
+//     'cvv': '123'
+//     'number': '4111111111111111',
+//     'expiration_year': '2016'
+//   }
+// });
 
-card.associate_to_customer(customer)
-    .debit(5000)
-    .then(function (debit) {
-        // save the result of the debit
-    }, function (err) {
-        // record the error message
-    });;
+// card.associate_to_customer(customer)
+//   .debit(5000)
+//   .then(function (debit) {
+//       // save the result of the debit
+//     console.log('debit result', debit);
+//     // console.log('card', debit._api.objects);
+//   }, function (err) {
+//       // record the error message
+//     console.log(err);
+//   });
