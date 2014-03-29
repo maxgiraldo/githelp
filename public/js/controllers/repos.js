70e28d2886a90d5a0629e60645806188b67c3a33
Max Giraldo <dr.datacode@gmail.com>
2014-03-29 17:36:15 -0400

    Fixed repo pages. Still need top contributor data

--- public/js/controllers/repos.js
+++ public/js/controllers/repos.js
@@ -2,6 +2,7 @@ angular.module('githelp.controllers.repos', [])
   .controller('ReposController', ['$scope', '$state', '$http', '$stateParams', 'Global', 'User', 'Inbox',
     function ($scope, $state, $http, $stateParams, Global, User, Inbox) {
     $scope.global = Global;
+    alert('public repos loaded');
     $scope.repoName = $stateParams.repoName;
     }
 ]);
