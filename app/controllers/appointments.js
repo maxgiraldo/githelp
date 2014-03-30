4d9048115c5e670aafe160fc07285cf57910efe8
Ji Ho Koo <jihokoo@gmail.com>
2014-03-30 04:25:06 -0400

    appointment routes

--- app/controllers/appointments.js
+++ app/controllers/appointments.js
@@ -83,9 +83,13 @@ var sendMessage = function(appointmentId, message, user){
 
 exports.toSession = function(req, res){
   Appointment.findOne({_id: req.params.appointmentId}, function(err, appointment){
-    res.render('sessionPage', {appointment});
+    res.render('sessionPage', {appointment: appointment});
   })
 }
+
+exports.confirmPage = function(req, res){
+  res.render('/confirm', {appointmentId: req.params.appointmentId})
+};
 
 exports.confirm = function(req, res) {
   // var id = "id of appointment";
