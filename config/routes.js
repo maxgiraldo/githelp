376be0eda6cff76e5c2b7b0f1dbb56efc974c78b
Ji Ho Koo <jihokoo@gmail.com>
2014-04-12 18:36:24 -0400

    minor changes

--- config/routes.js
+++ config/routes.js
@@ -26,7 +26,7 @@ passport.use(new GitHubStrategy({
   // Below are JHK's Keys
   clientID: '71778e134296a29071f4',
   clientSecret: '2a6a040b9fd4a2b74763055c8f017dba964f1d99',
-  callbackURL: "http://172.18.74.30:3000/auth/github/callback"
+  callbackURL: "http://172.18.75.131:3000/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({ githubId: profile.id }, function (err, user) {
