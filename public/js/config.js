4e713f63640b11e77ed6fcefe464c155ce02f9e9
Waine Tam <wainetam@gmail.com>
2014-04-12 18:43:33 -0400

    moved checkLoggedIn

--- public/js/config.js
+++ public/js/config.js
@@ -1,29 +1,6 @@
 //Setting up route
 window.app.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-
-        // for client-side authentication
-        var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
-          // Initialize a new promise
-          var deferred = $q.defer();
-          // Make an AJAX call to check if the user is logged in
-          $http.get('/loggedin').success(function(user){
-            // Authenticated
-            if (user !== '0')
-              $timeout(deferred.resolve, 0);
-            // Not Authenticated
-            else {
-              $rootScope.message = 'You need to log in.';
-              $timeout(function() {
-                deferred.reject();
-              }, 0);
-              $location.url('/signin');
-            }
-          });
-          return deferred.promise;
-        };
-        // end
-
         // start state params ---
         $urlRouterProvider.otherwise('/');
 
@@ -102,6 +79,28 @@ window.app.config(['$stateProvider', '$urlRouterProvider',
           });
     }
 ]);
+
+  // for client-side authentication
+  function checkLoggedin ($q, $timeout, $http, $location, $rootScope) {
+    // Initialize a new promise
+    var deferred = $q.defer();
+    // Make an AJAX call to check if the user is logged in
+    $http.get('/loggedin').success(function(user){
+      // Authenticated
+      if (user !== '0')
+        $timeout(deferred.resolve, 0);
+      // Not Authenticated
+      else {
+        $rootScope.message = 'You need to log in.';
+        $timeout(function() {
+          deferred.reject();
+        }, 0);
+        $location.url('/signin');
+      }
+    });
+    return deferred.promise;
+  };
+  // end
 
 //Setting HTML5 Location Mode
 window.app.config(['$locationProvider', 'datepickerConfig', 'datepickerPopupConfig',
